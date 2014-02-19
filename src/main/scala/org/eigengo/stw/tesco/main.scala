c256ee995ac783f20721579b33b67bc351ceb875
Jan Machacek <jan.machacek@gmail.com>
2014-02-19 09:52:42 +0000

    Removed code for the workshop

--- src/main/scala/org/eigengo/stw/tesco/main.scala
+++ src/main/scala/org/eigengo/stw/tesco/main.scala
@@ -8,44 +8,5 @@ import scala.annotation.tailrec
 object Main extends App {
   import akka.actor.ActorDSL._
   import TescoApiActor._
-  implicit val system = ActorSystem()
-
-  implicit val _ = actor(new Act {
-    become {
-      case x => println(x)
-    }
-  })
-  val uri = Uri("http://www.techfortesco.com/groceryapi_b1/restservice.aspx")
-  val credentials = {
-    // the ~/.tescoapi file should contain four lines:
-    //
-    // email address
-    // password
-    // developer key
-    // application key
-    //
-    val lines = Source.fromFile(System.getProperty("user.home") + "/.tescoapi").getLines().toList
-    Credentials(lines(0), lines(1), lines(2), lines(3))
-  }
-  val api = system.actorOf(Props(new TescoApiActor(uri, credentials) with RealApiAuthentication))
-
-  val SearchCommand     = "find (\\d+)? (.*)".r
-  val ListOffersCommand = "offers (\\d+)?".r
-
-  /**
-   * Main command loop
-   */
-  @tailrec
-  def commandLoop(): Unit = {
-    Console.readLine() match {
-      case "exit" => return
-      case SearchCommand(page, query) => api ! Search(query, if (page.isEmpty) None else Some(page.toInt))
-      case ListOffersCommand(page)    => api ! ListOffers(if (page.isEmpty) None else Some(page.toInt))
-    }
-
-    commandLoop()
-  }
-
-  commandLoop()
 
 }
